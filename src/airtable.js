0af9185958c6a4a9a11e761a301c963e32e85a14
Kris <kriistjanm@gmail.com>
2021-11-22 16:03:31 +0700

    Add CRON, adjust code organization

--- src/airtable.js
+++ src/airtable.js
@@ -1,11 +1,11 @@
 // Make a URL for the Airtable API
-export function airtableUrl(baseName, offset = null) {
+function airtableUrl(baseName, offset = null) {
     let append = offset ? `?offset=${offset}` : ''
     return `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${baseName}${append}`
 }
 
 // Minify the data from the Airtable API by removing bloat
-export function minifyAirtableData(data) {
+function minifyAirtableData(data) {
 
     let minified = []
 
@@ -14,6 +14,38 @@ export function minifyAirtableData(data) {
         if (field.Link) delete field.Link
         minified = minified.concat(field)
     })
+
+    return minified
+}
+
+export async function fetchBase(baseName) {
+    let url = airtableUrl(baseName)
+
+    let data = []
+    let finished = false
+
+    while (!finished) {
+        let response = await fetch(url, {
+            headers: {
+                'Authorization': `Bearer ${AIRTABLE_API_KEY}`
+            }
+        })
+
+        let json = await response.json()
+        data = data.concat(json.records)
+
+        if (json.offset) {
+            url = airtableUrl(baseName, json.offset)
+        } else {
+            finished = true
+        }
+    }
+
+    let minified = minifyAirtableData(data)
+
+    // Save to KV store
+    await Index.put(baseName, JSON.stringify(minified))
+    await Index.put(`${baseName}_lastUpdated`, new Date().toISOString())
 
     return minified
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import { airtableUrl, minifyAirtableData } from './airtable'
+import { fetchBase } from './airtable'
 
 async function handler(request) {
 
     // get the query params
     const { searchParams } = new URL(request.url)
-    const list = searchParams.get('list') // The type of index to fetch
-    const key = searchParams.get('key') // The key to verify the request
+    const indexName = searchParams.get('list') // The type of index to fetch (ETFs)
     const forceUpdate = searchParams.get('forceUpdate') // Force an update of the index
+    const key = searchParams.get('key') // The key to verify the request
 
     // make sure all params are present and valid
-    if (!list || key !== SA_CF_KEY) return new Response('Problem', { status: 400 })
-
-    // fetch the stored data and timestamp
-    const stored = await Index.get(list)
-    const updated = stored ? await Index.get(`${list}_lastUpdated`) : 0
-
-    // get minutes elapsed since the last update
-    const minutes = (Date.now() - new Date(updated)) / 60000
+    if (!indexName || key !== SA_CF_KEY) return new Response('Problem', { status: 400 })
 
     // if there is data and it's fresh, return it (else, fetch it again)
     if (!forceUpdate) {
+
+        // fetch the stored data and timestamp
+        const stored = await Index.get(indexName)
+        const updated = stored ? await Index.get(`${indexName}_lastUpdated`) : 0
+
+        // get minutes elapsed since the last update
+        const minutes = (Date.now() - new Date(updated)) / 60000
+
         if (stored && minutes < 60) {
             return new Response(stored, {
                 headers: {
@@ -29,35 +30,9 @@ async function handler(request) {
         }
     }
 
-    let url = airtableUrl(list)
+    let data = await fetchBase(indexName)
 
-    let data = []
-    let finished = false
-
-    while (!finished) {
-        let response = await fetch(url, {
-            headers: {
-                'Authorization': `Bearer ${AIRTABLE_API_KEY}`
-            }
-        })
-
-        let json = await response.json()
-        data = data.concat(json.records)
-
-        if (json.offset) {
-            url = airtableUrl(list, json.offset)
-        } else {
-            finished = true
-        }
-    }
-
-    let minified = minifyAirtableData(data)
-
-    // Save to KV store
-    await Index.put(list, JSON.stringify(minified))
-    await Index.put(`${list}_lastUpdated`, new Date().toISOString())
-
-    return new Response(JSON.stringify(minified), {
+    return new Response(JSON.stringify(data), {
         headers: {
             'Content-Type': 'application/json'
         }
@@ -65,6 +40,10 @@ async function handler(request) {
 }
 
 
+
+async function updateAllIndexes() {
+    await fetchBase('ETFs')
+}
 
 
 
@@ -76,4 +55,8 @@ async function handler(request) {
 
 addEventListener('fetch', event => {
     event.respondWith(handler(event.request))
+})
+
+addEventListener('scheduled', event => {
+    event.waitUntil(updateAllIndexes(event))
 })
